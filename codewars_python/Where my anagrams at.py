bb76ad793c4d42467c9564708bbaf59af4e76008
wen1ling <m17639613778@163.com>
2021-10-12 00:10:00 +0800

    codewarsday6.1

---
+++ codewars_python/Where my anagrams at.py
@@ -0,0 +1,23 @@
+"""
+问题描述
+https://www.codewars.com/kata/55c04b4cc56a697bb0000048
+"""
+
+
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+def anagrams(word, words):
+    new_words = words[:]
+    for i in range(0, len(words)):
+        if len(word) != len(words[i]):
+            new_words.remove(words[i])
+        else:
+            for j in range(97, 123):
+                if int(chr(j) in word) ^ int(chr(j) in words[i]) == 1:
+                    if words[i] in new_words:
+                        new_words.remove(words[i])
+    return new_words
+
+
+print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
